735ab7044f960893fb2a570a8c4a238b07f1ab74
DverkaSK <DverkaSK@gmail.com>
2024-02-26 10:53:35 +0300

    Refactor code #3

--- api/src/main/java/ru/dverkask/skinanatomy/api/SkinAnatomy.java
+++ api/src/main/java/ru/dverkask/skinanatomy/api/SkinAnatomy.java
@@ -1,12 +1,10 @@
 package ru.dverkask.skinanatomy.api;
 
-import org.bukkit.entity.Player;
-
 import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
 
-public interface ISkinAnatomyAPI {
+public interface SkinAnatomy {
     Map<UUID, String> getSkins();
     void addCustomSkin(UUID uuid, String url);
     SkinDecomposer createSkinDecomposer(String url) throws IOException;
--- api/src/main/java/ru/dverkask/skinanatomy/api/SkinAnatomyAPI.java
+++ api/src/main/java/ru/dverkask/skinanatomy/api/SkinAnatomyAPI.java
@@ -2,24 +2,23 @@ package ru.dverkask.skinanatomy.api;
 
 import lombok.Getter;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import org.bukkit.entity.Player;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-public class SkinAnatomyAPI {
-    @Getter
-    private final static Map<UUID, String> skins = new HashMap<>();
-    public static void addCustomSkin(@NonNull UUID uuid,
-                                     @NonNull String url) {
+@Getter
+public enum SkinAnatomyAPI implements SkinAnatomy {
+    INSTANCE;
+    private final Map<UUID, String> skins = new HashMap<>();
+
+    public void addCustomSkin(@NonNull UUID uuid,
+                              @NonNull String url) {
         skins.put(uuid, url);
     }
 
-    public static SkinDecomposer createSkinDecomposer(@NonNull String url) throws IOException {
+    public SkinDecomposer createSkinDecomposer(@NonNull String url) throws IOException {
         return new SkinDecomposer(url);
     }
 }
